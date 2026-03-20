47daecf276fd9590e6ca9ffa4e4f378cc6a363ca
angelestrella1313-web <angelestrella1313@gmail.com>
2026-03-20 18:18:18 -0500

    Update page.tsx

--- app/photos/page.tsx
+++ app/photos/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import BottomNav from "@/components/BottomNav";
+import BottomNav from "../../components/BottomNav";
 export default function PhotosPage() {
   return (
     <main className="min-h-screen bg-[#f7f4ef] pb-24">
