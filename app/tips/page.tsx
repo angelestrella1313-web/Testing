e7ab2997d8e1a1add546a4e66d277bf59ae73e4e
angelestrella1313-web <angelestrella1313@gmail.com>
2026-03-20 18:25:39 -0500

    Update page.tsx

--- app/tips/page.tsx
+++ app/tips/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import BottomNav from "../components/BottomNav";
+import BottomNav from "../../components/BottomNav";
 import { useState } from "react";
 import Link from "next/link";
 import {
