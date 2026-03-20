5f888d33b396550f07654547cb0402266bd65e85
angelestrella1313-web <angelestrella1313@gmail.com>
2026-03-20 18:25:20 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import Link from "next/link";
-import BottomNav from "../components/BottomNav";
+import BottomNav from "../../components/BottomNav";
 import {
   Settings,
   ChevronDown,
